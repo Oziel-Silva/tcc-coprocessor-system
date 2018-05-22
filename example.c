3aeb1883f8e326957a4f044bcb41ae990f78541e
Oziel-Silva <oziel.service@live.com>
2018-05-21 21:46:39 -0300

    up file

--- example.c
+++ example.c
@@ -64,25 +64,34 @@ int main(int argc, char *argv[])
 
     /* Now the file is ready to be mmapped.
      */
-    map = mmap(0, MAP_RANGE, PROT_READ | PROT_WRITE, MAP_SHARED, fd,MAP_BASE);
-    if (map == MAP_FAILED) {
-	close(fd);
-	perror("Error mmapping the file");
-	exit(EXIT_FAILURE);
-    }
+    //map = mmap(0, MAP_RANGE, PROT_READ | PROT_WRITE, MAP_SHARED, fd,MAP_BASE);
+ //    if (map == MAP_FAILED) {
+	// close(fd);
+	// perror("Error mmapping the file");
+	// exit(EXIT_FAILURE);
+ //    }
     
-    /* Now write int's to the file as if it were memory (an array of ints).
-     */
-	//for (i = 1 ; i < 100; i++)
-	map[0] = 0xB;
+    //Now write int's to the file as if it were memory (an array of ints).
+    char buffer[32];
+    int resposta;
+    char *word[3];
+
+    FILE *file;
+
+    file = fopen("file.txt","r");
+    fgets(buffer,32,file);
+    printf("%s\n",buffer );
+    word[0] = buffer;
+    printf("%s\n",word[0]);   
+	//map[0] = 0xB;
 
     /* Don't forget to free the mmapped memory
      */
-    if (munmap(map, FILESIZE) == -1) {
-	perror("Error un-mmapping the file");
+   if (munmap(map, FILESIZE) == -1) {
+	//perror("Error un-mmapping the file");
 	/* Decide here whether to close(fd) and exit() or not. Depends... */
-    }
-
+    
+   }
     /* Un-mmaping doesn't close the file, so we still need to do that.
      */
     close(fd);
